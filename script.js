72555ec25303bcbae341e34e631ed1d85593ebfe
SnappyCarp <126493503+SnappyCarp@users.noreply.github.com>
2023-06-26 17:24:37 -0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var DownloadBtn = document.getElementById('download')
-var urlIn = document.getElementById('input')
-
-DownloadBtn.addEventListener('click', function() {
-    console.log(`Download Triggered With: ${urlIn.value}`)
-    
-});
