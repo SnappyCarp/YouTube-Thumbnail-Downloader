61a63e7a7f1c5c7b77d61cea48636627513c31f9
SnappyCarp <126493503+SnappyCarp@users.noreply.github.com>
2023-06-26 17:20:46 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var DownloadBtn = document.getElementById('download')
+var urlIn = document.getElementById('input')
+
+DownloadBtn.addEventListener('click', function() {
+    console.log(`Download Triggered With: ${urlIn.value}`)
+    
+});
